a6da5eec4005f2c7c05d7576980466a2b2b8ef33
rarit0507 <yrl05070875@gmail.com>
2024-03-06 09:01:06 +0900

    Generic & Map

--- test12/src/sec1/Generic1.java
+++ test12/src/sec1/Generic1.java
@@ -1,7 +1,7 @@
 package sec1;
 //제네릭(Generic)이란? 데이터와 관계없이 아무거나 모두 받아서 처리하는 클래스
-//자주 사용하는 타입 인자 : T(type), E(Element)
-public class Generic1<T> {	//<T>:타입 관계X
+//자주 사용하는 타입 인자 : T(type), E(Element), K(Key), V(Value), N(Number), R(Result)
+public class Generic1<T> {	//<T>:안에 뭘 쓰든 상관 없으나 관례임
 	private T field1;
 	//Constructor
 	public Generic1() {}
